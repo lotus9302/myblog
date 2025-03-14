58029bc9707b36f43eb18efe1d88f1adfd9445d3
AyakaHamasaki <kjsycz@gmail.com>
2025-03-14 10:21:28 +0000

    カリキュラムに沿ったmigrationに修正

--- database/migrations/2025_03_13_105246_create_blog_table.php
+++ database/migrations/2025_03_13_105246_create_blog_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('blog', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->timestamp('image_path')->nullable();
             $table->timestamps();
         });
     }
